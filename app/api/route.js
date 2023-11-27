989a3c5de67abc8f0b4ae8794dfdd90b236ff460
pythonthm <pythonthm@gmail.com>
2023-11-27 15:02:39 +0900

    add-p

--- app/api/route.js
+++ app/api/route.js
@@ -1,4 +1,10 @@
 import { NextResponse } from "next/server";
 export async function GET() {
-  return NextResponse.json({ data: "Hello Next.js 13 !!" });
+  return NextResponse.json([
+    { data: "This is DATA 1", message: "Hello Next.js 13 !!" },
+
+    { data: "This is DATA 2", message: "Hello NextResponse !!" },
+
+    { data: "This is DATA 3", message: "Hello API router.js !!" },
+  ]);
 }
